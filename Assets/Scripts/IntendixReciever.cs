0e714eed8c627a60bd816afa3414575905b07228
Adam Gotlib <gotlib.adam@gmail.com>
2018-04-22 08:25:26 +0200

    Handle input from IndendiX (via transmitter)

--- Assets/Scripts/IntendixReciever.cs
+++ Assets/Scripts/IntendixReciever.cs
@@ -1,43 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Net;
+using System.Net.Sockets;
+
 using UnityEngine;
 
 public class IntendixReciever : MonoBehaviour {
-
-    [SerializeField]
     HelicopterController _helicopterController;
 
-    private void Start()
-    {
+    IPEndPoint _endpoint;
+    UdpClient _udpClient;
+
+    public string turnLeft;
+    public string turnRight;
+
+    private void Start() {
         _helicopterController = GetComponent<HelicopterController>();
+
+        _endpoint = new IPEndPoint(IPAddress.Any, 1001);
+        _udpClient = new UdpClient(_endpoint);
     }
 
     private void Update()
     {
-        ProcessIntendixInput();
+        while (_udpClient.Available > 0) {
+            byte[] buffer = _udpClient.Receive(ref _endpoint);
+
+            string command = System.Text.Encoding.Default.GetString(buffer);
+            Debug.Log(command);
+            HandleCommand(command);
+            //Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+            //newsock.Send(data, data.Length, sender);
+        }
+
+        
     }
 
-    void ProcessIntendixInput()
+    private void HandleCommand(string command)
     {
-        //Replace keyboard input with intendix one
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            _helicopterController.MaximizeEngineForce();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _helicopterController.TurnOffEngine();
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            _helicopterController.ManualTurnRight();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
+        if (command == turnLeft) {
+            Debug.Log("Turning left");
             _helicopterController.ManualTurnLeft();
+        }
+        
+        if (command == turnRight) {
+            Debug.Log("Turning right");
+            _helicopterController.ManualTurnRight();
         }
     }
 }
