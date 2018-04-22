52fbf85bfb93937ce51bb51975f243656d06484c
potrykus.g <potrykus.g@evehor.com>
2018-04-22 05:34:17 +0200

    Added working Helicopter.
    Added IntendixReciever (to override with real input)

---
+++ Assets/Scripts/IntendixReciever.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntendixReciever : MonoBehaviour {
+
+    [SerializeField]
+    HelicopterController _helicopterController;
+
+    private void Update()
+    {
+        ProcessIntendixInput();
+    }
+
+    void ProcessIntendixInput()
+    {
+        //Replace keyboard input with intendix one
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _helicopterController.MaximizeEngineForce();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _helicopterController.TurnOffEngine();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _helicopterController.ManualTurnRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _helicopterController.ManualTurnLeft();
+        }
+    }
+}
